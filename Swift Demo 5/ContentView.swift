1c904744e0f4203caa572028dfa0ef9c0ee756f8
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-05-08 05:03:07 -0700

    adding random numbers

--- Swift Demo 5/ContentView.swift
+++ Swift Demo 5/ContentView.swift
@@ -9,6 +9,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var symbols = ["apple", "star", "cherry"]
+    @State private var numbers = [0, 0, 0]
     @State private var credits = 1000
     var body: some View {
         ZStack {
@@ -45,17 +47,17 @@ struct ContentView: View {
 //                cards
                 HStack {
                     Spacer()
-                    Image("apple")
+                    Image(symbols[numbers[0]])
                         .resizable()
                         .aspectRatio(1, contentMode: .fit)
                         .background(Color.white.opacity(0.5))
                         .cornerRadius(20)
-                    Image("apple")
+                    Image(symbols[numbers[1]])
                         .resizable()
                         .aspectRatio(1, contentMode: .fit)
                         .background(Color.white.opacity(0.5))
                         .cornerRadius(20)
-                    Image("apple")
+                    Image(symbols[numbers[2]])
                         .resizable()
                         .aspectRatio(1, contentMode: .fit)
                         .background(Color.white.opacity(0.5))
@@ -66,7 +68,9 @@ struct ContentView: View {
                 Spacer()
 //                button
                 Button(action: {
-                    self.credits += 1
+                    self.numbers[0] = Int.random(in: 0...self.symbols.count - 1)
+                    self.numbers[1] = Int.random(in: 0...self.symbols.count - 1)
+                    self.numbers[2] = Int.random(in: 0...self.symbols.count - 1)
                 }) {
                     Text("Spin")
                         .bold()
